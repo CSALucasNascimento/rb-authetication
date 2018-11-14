dbe97ab86b1eb9f4465c1f7d17ad7c5d2bd0abdf
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-11-14 18:16:06 +1100

    first commit

---
+++ src/Authentication/Authentication.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import './Authentication.scss'
+import * as logo from './logo.png'
+
+class Authentication extends React.Component {
+
+  render() {
+    return (
+      <div className='authentication'>
+      Auth
+        {/* <img src={logo} className='logo'/> */}
+      </div>
+    )
+  }
+
+}
+
+export default Authentication
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from "react-dom"
+
+import Authentication from './Authentication/Authentication'
+
+ReactDOM.render(<Authentication />, document.getElementById('root'))
